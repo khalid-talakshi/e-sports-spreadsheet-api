64f9834531002e1943224d597a1b69eea016aecb
Khalid Talakshi <khalid.talakshi@outlook.com>
2022-04-24 23:31:06 -0400

    added endpoint to get modified time

--- src/index.js
+++ src/index.js
@@ -10,15 +10,17 @@ dotenv.config()
 
 app.use(cors())
 
+const auth = await google.auth.getClient({
+    scopes: ['https://www.googleapis.com/auth/spreadsheets.readonly', 'https://www.googleapis.com/auth/drive.metadata.readonly'],
+    keyFile: process.env.GOOGLE_APPLICATION_CREDENTIALS
+});
+
 // define a route handler for the default home page
 app.get("/", (req, res) => {
     res.send("Hello world!");
 });
 
 app.get("/schedule", async(req, res) => {
-    // Auth
-    const auth = await google.auth.getClient({ scopes: ['https://www.googleapis.com/auth/spreadsheets.readonly'], keyFile: process.env.GOOGLE_APPLICATION_CREDENTIALS });
-
     const sheets = google.sheets({ version: 'v4', auth });
 
     const range = `Sheet1!A1`;
@@ -33,6 +35,19 @@ app.get("/schedule", async(req, res) => {
     console.log(response.data.values[0])
     res.send({ data: response.data.values[0] })
 
+})
+
+app.get("/modifiedDate", async(req, res) => {
+    const drive = google.drive({ version: "v3", auth })
+
+    const response = await drive.files.get({
+        fileId: process.env.SHEET_ID,
+        fields: "modifiedTime"
+    })
+
+    console.log(response.data)
+
+    res.send(response.data)
 })
 
 
