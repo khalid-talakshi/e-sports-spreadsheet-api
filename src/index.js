e2d081d1c80eb2f83d64e44816117059ba11d49c
Khalid Talakshi <khalid.talakshi@outlook.com>
2022-04-25 00:42:30 -0400

    changed data format for schedule api

--- src/index.js
+++ src/index.js
@@ -7,13 +7,22 @@ const app = express();
 const port = process.env.PORT || 8080; // default port to listen
 dotenv.config()
 
-
 app.use(cors())
 
 const auth = await google.auth.getClient({
     scopes: ['https://www.googleapis.com/auth/spreadsheets.readonly', 'https://www.googleapis.com/auth/drive.metadata.readonly'],
     keyFile: process.env.GOOGLE_APPLICATION_CREDENTIALS
 });
+
+const drive = google.drive({ version: "v3", auth })
+
+const getLastModifiedDate = async() => {
+    const response = await drive.files.get({
+        fileId: process.env.SHEET_ID,
+        fields: "modifiedTime"
+    })
+    return response
+}
 
 // define a route handler for the default home page
 app.get("/", (req, res) => {
@@ -23,29 +32,47 @@ app.get("/", (req, res) => {
 app.get("/schedule", async(req, res) => {
     const sheets = google.sheets({ version: 'v4', auth });
 
-    const range = `Sheet1!A1`;
+    const range = `Sheet1!A1:E18`;
 
     const response = await sheets.spreadsheets.values.get({
         spreadsheetId: process.env.SHEET_ID,
         range,
     });
 
-    // Result
+    const lastModifiedDate = await getLastModifiedDate()
 
-    console.log(response.data.values[0])
-    res.send({ data: response.data.values[0] })
+    const values = response.data.values
 
+    let day1;
+    let day2;
+    let day3;
+    let result = { "friday": {}, "saturday": {} }
+
+    for (let i = 0; i < values.length; i++) {
+        if (i == 0) {
+            // day row
+            continue;
+        } else if (i == 1) {
+            // stream row
+            continue;
+        } else {
+            // standard time row
+            const time = values[i][0]
+            const stream1Friday = values[i][1] || ""
+            const stream2Friday = values[i][2] || ""
+            const stream1Saturday = values[i][3] || ""
+            const stream2Saturday = values[i][4] || ""
+            result["friday"] = {...result["friday"], [time]: [stream1Friday, stream2Friday] }
+            result["saturday"] = {...result["saturday"], [time]: [stream1Saturday, stream2Saturday] }
+        }
+    }
+
+    res.send({ data: result, "lastModifiedDate": lastModifiedDate.data["modifiedTime"] })
 })
 
 app.get("/modifiedDate", async(req, res) => {
-    const drive = google.drive({ version: "v3", auth })
 
-    const response = await drive.files.get({
-        fileId: process.env.SHEET_ID,
-        fields: "modifiedTime"
-    })
-
-    console.log(response.data)
+    const response = await getLastModifiedDate()
 
     res.send(response.data)
 })
